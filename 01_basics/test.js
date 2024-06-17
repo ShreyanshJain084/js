26d686784dca9b96fca8181d58b40965eb557005
Shreyansh <125195482+ratatoulli@users.noreply.github.com>
2024-06-17 12:27:54 +0000

    JASON TALK

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,17 @@
-console.log("SHREYANSH")
+// const obj1 = {1: "a", 2: "b"}
+// const obj2 = {3: "a", 4: "b"}
+// const obj4 = {5: "a", 6: "b"}
+
+// // const obj3 = { obj1, obj2 }
+// // const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+// const obj3 = {...obj1, ...obj2}
+//  console.log(obj3);
+//  console.log(obj1);
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+const arr=Object.keys(tinderUser);
+//console.log(arr)
